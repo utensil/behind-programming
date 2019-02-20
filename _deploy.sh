b756de55923b4c9fcbe61de525c1796e7622997d
utensil <utensilcandel@gmail.com>
2019-02-20 21:29:40 +0800

    Fix typo

--- _deploy.sh
+++ _deploy.sh
@@ -5,8 +5,8 @@ set -e
 [ -z "${GITHUB_PAT}" ] && exit 0
 [ "${TRAVIS_BRANCH}" != "bookdown" ] && exit 0
 
-git config --global user.email ${GITHUB_USER}
-git config --global user.name ${GITHUB_EMAIL}
+git config --global user.email ${GITHUB_EMAIL}
+git config --global user.name ${GITHUB_USER}
 
 git clone -b gh-pages https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git book-output
 cd book-output
